1f332d8524e8f23486a810c07a2e892a6ba3531c
Pietro Giordano <giopi87@gmail.com>
2023-02-13 22:15:37 +0100

    fine header con jumbotron

--- src/components/Header/AppHeader.vue
+++ src/components/Header/AppHeader.vue
@@ -19,6 +19,87 @@ export default {
       </header>
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
+header {
+      @include bg-image('../../assets/homepage-hero-background.jpg');
+      height: 600px;
 
+      .phone-contact {
+            background-color: $hard-grey;
+
+            .container {
+                  @include container;
+                  @include flex(row, space-between);
+                  height: 35px;
+
+                  p {
+                        color: $medium-grey;
+                        font-size: 0.6rem;
+                        line-height: 35px;
+                  }
+            }
+      }
+
+      .nav-bar {
+            background-color: rgba($color: $hard-grey, $alpha: 0.5);
+
+            .container {
+                  @include container;
+                  @include flex(row, space-between, center);
+                  height: 95px;
+
+                  div:last-child {
+                        @include flex;
+
+                        ul {
+                              @include flex(row, space-between, center);
+
+                              a {
+                                    text-transform: uppercase;
+                                    font-weight: bold;
+                                    color: white;
+                                    padding: 5px 0;
+                                    font-size: 0.8rem;
+                                    margin-right: 35px;
+
+                                    &.active {
+                                          color: $main-green;
+                                          border-bottom: 2px solid $main-green;
+                                    }
+
+                                    span {
+                                          background-color: $main-green;
+                                          font-size: 0.6rem;
+                                          padding: 3px;
+                                          border-radius: 5px;
+                                    }
+                              }
+                        }
+
+                        button {
+                              @include button;
+                        }
+                  }
+            }
+      }
+
+      .jumbotron {
+            @include container;
+            @include flex(column, center, flex-start);
+            height: 470px;
+            color: white;
+
+            >* {
+                  padding: 8px 0;
+            }
+
+            h1 {
+                  font-size: 3rem;
+            }
+
+            p {
+                  font-size: 1.3rem;
+            }
+      }
+}
 </style>
--- src/components/Header/AppJumbotron.vue
+++ src/components/Header/AppJumbotron.vue
@@ -5,7 +5,10 @@ export default {
 </script>
 
 <template>
-
+      <div class="jumbotron">
+            <h1>Drive with Avada</h1>
+            <p>We offer the finest driving tuition money can buy</p>
+      </div>
 </template>
 
 <style lang="scss" scoped>
