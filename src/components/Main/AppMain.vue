450f44a5be3f49bca6eff413ce105db76fb5d193
Pietro Giordano <giopi87@gmail.com>
2023-02-14 11:59:49 +0100

    avanzamento testimonials

--- src/components/Main/AppMain.vue
+++ src/components/Main/AppMain.vue
@@ -1,13 +1,88 @@
 <script>
+import Testimonials from './Testimonials.vue';
+
 export default {
       name: 'AppMain',
+      components: {
+            Testimonials,
+      },
+      data() {
+            return {
+                  testimonials: [
+                        {
+                              name: 'Sophia Jones',
+                              photo: 'testimonial-sophia.png',
+                              text: 'Avada Driving School really helped build my confidence behind the wheel and with driving in general, and they got me a first time pass! Highly recommended.',
+                              selected: true
+                        }, {
+                              name: 'Grant Smith',
+                              photo: 'testimonial-grant.png',
+                              text: 'Avada Driving School really helped build my confidence behind the wheel and with driving in general, and they got me a first time pass! Highly recommended.',
+                              selected: false
+                        }, {
+                              name: 'Harold White',
+                              photo: 'testimonial-harold.png',
+                              text: 'Avada Driving School really helped build my confidence behind the wheel and with driving in general, and they got me a first time pass! Highly recommended.',
+                              selected: false
+                        }, {
+                              name: 'Kate Middleton',
+                              photo: 'testimonial-kate.png',
+                              text: 'Avada Driving School really helped build my confidence behind the wheel and with driving in general, and they got me a first time pass! Highly recommended.',
+                              selected: false
+                        }, {
+                              name: 'Kelly Clarkson',
+                              photo: 'testimonial-kelly.png',
+                              text: 'Avada Driving School really helped build my confidence behind the wheel and with driving in general, and they got me a first time pass! Highly recommended.',
+                              selected: false
+                        },
+                  ]
+            }
+      },
+      methods: {
+
+            changeTestimonial(index) {
+                  for (let i = 0; i < this.testimonials.length; i++) {
+                        this.testimonials[i].selected = false;
+                  }
+                  this.testimonials[index].selected = true;
+            }
+      }
 }
 </script>
 
 <template>
 
+      <section id="testimonials">
+            <Testimonials :testimonial="testimonial" v-for="testimonial in testimonials" />
+
+            <div class="point-container">
+                  <div class="point" v-for="(testimonial, index) in testimonials"
+                        :class="testimonial.selected ? 'point-selected' : ''" @click="changeTestimonial(index)"></div>
+            </div>
+      </section>
+
 </template>
 
 <style lang="scss" scoped>
+#testimonials {
+      @include bg-image('../../assets/testimonial-background.jpg');
+      @include flex(column, center, center);
+      height: 400px;
 
+      .point-container {
+            @include flex(row, center);
+
+            .point {
+                  width: 20px;
+                  height: 20px;
+                  border-radius: 50%;
+                  border: 1px solid $medium-grey;
+                  cursor: pointer;
+
+                  &.point-selected {
+                        background-color: $medium-grey;
+                  }
+            }
+      }
+}
 </style>
---
+++ src/components/Main/Testimonials.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+      name: 'Testimonials',
+      props: {
+            testimonial: Object
+      },
+      methods: {
+
+            getImgPath: function (imgPath) {
+                  return new URL(imgPath, import.meta.url).href;
+            }
+      }
+}
+</script>
+
+<template>
+      <div class="testimonials" v-if="testimonial.selected">
+            <h2>Testimonials</h2>
+            <p>Here's what our happy drivers had to say about our services:</p>
+
+            <div class="testimonials-photo">
+                  <img :src="getImgPath(`../../assets/${testimonial.photo}`)" alt="">
+            </div>
+
+            <p>{{ testimonial.text }}</p>
+            <p>{{ testimonial.name }}</p>
+      </div>
+
+</template>
+
+<style lang="scss">
+.testimonials {
+      @include container;
+      @include flex(column, center, center);
+
+      &-photo {
+            width: 100px;
+            height: 100px;
+
+            img {
+                  width: 100%;
+            }
+
+      }
+}
+</style>
